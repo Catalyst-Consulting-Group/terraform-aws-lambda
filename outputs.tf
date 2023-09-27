a671545b57658505bcbc92203f1e1d8e503aa6a6
Derek McKinnon <mckinnon.derek@gmail.com>
2023-09-27 12:00:08 -0400

    Add `function_name` and `invoke_arn` outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,17 @@
+output "function_name" {
+  value = aws_lambda_function.this.function_name
+}
+
 output "arn" {
   value = aws_lambda_function.this.arn
 }
 
 output "role_arn" {
   value = aws_iam_role.this.arn
+}
+
+output "invoke_arn" {
+  value = aws_lambda_function.this.invoke_arn
 }
 
 output "log_group_name" {
